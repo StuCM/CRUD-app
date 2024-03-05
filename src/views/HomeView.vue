b70273b5d0716056e5b23adab6dbb26641428c80
Stuart Marshall <stucmarshall@gmail.com>
2024-03-05 14:19:36 +0000

    Make table scrollable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,129 +1,133 @@
 <script setup>
-import { getAllCustomers, deleteCustomer } from '@/services/CustomerDataService'
-import { onMounted, ref } from 'vue'
+import { getAllCustomers, deleteCustomer } from '@/services/CustomerDataService';
+import { onMounted, ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'vue-router';
 
-const customers = ref([])
+const customers = ref([]);
 
 onMounted(async () => {
-  customers.value = await getAllCustomers()
-  console.log(customers.value)
-})
+    customers.value = await getAllCustomers();
+});
 
 const getCustomersByName = async (event) => {
-  if (event.target.value === '') customers.value = await getAllCustomers();
-  else {
-    customers.value = customers.value.filter((customer) => {
-      return customer.name.toLowerCase().includes(event.target.value.toLowerCase())
-    })
-  }
-}
+    if (event.target.value === '') customers.value = await getAllCustomers();
+    else {
+        customers.value = customers.value.filter((customer) => {
+            return customer.name.toLowerCase().includes(event.target.value.toLowerCase());
+        });
+    }
+};
 
 const deleteUser = (id) => {
-  customers.value = customers.value.filter((customer) => {
-    return customer.id !== id
-  })
-  deleteCustomer(id);
-}
+    customers.value = customers.value.filter((customer) => {
+        return customer.id !== id;
+    });
+    deleteCustomer(id);
+};
 
 const router = useRouter();
 
 const editUser = (id) => {
-  console.log(id)
-  router.push({ name: 'add', params: { id } })
-}
-
+    router.push({ name: 'add', params: { id } });
+};
 </script>
 
 <template>
-  <main>
-    <div class="search-container" @input="getCustomersByName">
-      <input type="text" placeholder="Search" />
-    </div>
-
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Active</th>
-        <th>Actions</th>
-
-      </tr>
-      <tr v-for="customer in customers" :key="customer.id">
-        <td>{{ customer.name }}</td>
-        <td>{{ customer.email }}</td>
-        <td>{{ customer.phone }}</td>
-        <td>{{ customer.active }}</td>
-        <td>
-          <button class="icon-button" @click="editUser(customer.id)">
-            <FontAwesomeIcon class="icon icon-edit" :icon="faEdit" />
-          </button>
-          <button class="icon-button" @click="deleteUser(customer.id)">
-            <FontAwesomeIcon class="icon icon-trash" :icon="faTrash" />
-          </button>
-          
-        </td>
-        <td></td>
-      </tr>
-    </table>
-
-  </main>
+    <main>
+        <div class="search-container" @input="getCustomersByName">
+            <input type="text" placeholder="Search" />
+        </div>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Active</th>
+                    <th>Actions</th>
+                </thead>
+                <tr v-for="customer in customers" :key="customer.id">
+                    <td>{{ customer.name }}</td>
+                    <td>{{ customer.email }}</td>
+                    <td>{{ customer.phone }}</td>
+                    <td>{{ customer.active }}</td>
+                    <td>
+                        <button class="icon-button" @click="editUser(customer.id)">
+                            <FontAwesomeIcon class="icon icon-edit" :icon="faEdit" />
+                        </button>
+                        <button class="icon-button" @click="deleteUser(customer.id)">
+                            <FontAwesomeIcon class="icon icon-trash" :icon="faTrash" />
+                        </button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </main>
 </template>
 
 <style lang="scss">
 main {
-  width: 800px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+    width: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+}
+.table-container {
+    max-height: 550px;
+    overflow-y: auto;
 }
 table {
-  min-width: 100%;
-  border-collapse: collapse;
-  th {
-    background-color: #464646;
-    color: white;
-    padding: 1rem;
-  }
-  td {
-    padding: 1rem;
-    border-bottom: 1px solid #f1f1f1;
-  }
-  tr {
-    &:hover {
-      background-color: #f1f1f1;
+    min-width: 100%;
+    border-collapse: collapse;
+    thead {
+        position: sticky;
+        top: 0;
+        background-color: #464646;
     }
-  }
+    th {
+        background-color: #464646;
+        color: white;
+        padding: 1rem;
+    }
+    td {
+        padding: 1rem;
+        border-bottom: 1px solid #f1f1f1;
+    }
+    tr {
+        width: 100%;
+        &:hover {
+            background-color: #f1f1f1;
+        }
+    }
 }
 
 .search-container {
-  input {
-    padding: 1rem;
-    min-width: 300px;
-    border: 1px solid #bbbbbb;
-    border-radius: 40px;
-    margin-bottom: 1rem;
-  }
+    input {
+        padding: 1rem;
+        min-width: 300px;
+        border: 1px solid #bbbbbb;
+        border-radius: 40px;
+        margin-bottom: 1rem;
+    }
 }
 
 .icon {
-  cursor: pointer;
-  margin: 0 0.75rem;
-  &.icon-edit {
-    color: #1795c7;
-    &:hover {
-    color: #137aa3
-  }
-  }
-  &.icon-trash {
-    color: #e61313;
-    &:hover {
-      color: #be1010;
+    cursor: pointer;
+    margin: 0 0.75rem;
+    &.icon-edit {
+        color: #1795c7;
+        &:hover {
+            color: #137aa3;
+        }
     }
-  }
+    &.icon-trash {
+        color: #e61313;
+        &:hover {
+            color: #be1010;
+        }
+    }
 }
 </style>
